2703902a35e3cf1550ca0875daa8c48193554da2
ipaullly <ipaully22@gmail.com>
2020-01-05 23:40:12 +0300

    setup server and database

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+//initialize library to detect .env variables
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// const productRoutes = require('./api/routes/products');
+// const orderRoutes = require('./api/routes/orders');
+// const userRoutes = require('./api/routes/user');
+
+mongoose.connect(process.env.MONGO_URL_DEV, { useNewUrlParser: 'true'});
+
+// db error handling
+mongoose.connection.on('error', (err) => {
+    console.log('err', err);
+});
+mongoose.connection.on('connected', (err, res) => {
+    console.log('Mongoose is connected');
+});
+
+const app = express();
+
+// log request data
+app.use(morgan('dev'));
+
+// setup static files path
+app.use('/uploads', express.static('uploads'));
+
+// body parser middleware parses request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// setup CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Allow-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Headers', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+//Routes that handle requests
+// app.use('/products', productRoutes);
+// app.use('/orders', orderRoutes);
+// app.use('/user', userRoutes);
+
+//Error handling
+app.use((req, res, next) => {
+    const error = new Error();
+    error.message = 'Not Found at all';
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error,
+    });
+});
+
+module.exports = app
